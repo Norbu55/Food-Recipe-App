e2a3acc8d28e6086c0d9d91e2937657a45fbc499
Pasang Norbu Sherpa <uzumaki2357@gmail.com>
2022-06-13 11:21:13 +0545

    Food recipe App

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RecipeCard = ({ recipe }) => {
-  const { idMeal, strMeal, strCategory, strMealThumb } = recipe;
+  const [instruction, setInstruction] = useState(false);
+
+  const changeData = () => {
+    setInstruction(!instruction);
+  };
+
+  const { idMeal, strMeal, strCategory, strMealThumb, strInstructions } =
+    recipe;
+
+  const Results = () => (
+    <div id="results" className="search-results">
+      {strInstructions}
+    </div>
+  );
 
   return (
     <div className="card">
       <img src={strMealThumb} alt={strMeal} className="card-image" />
       <div className="card-body">
         <span className="category">{strCategory}</span>
+        <div>
+          <input type="submit" value="Instructions" onClick={changeData} />
+          {instruction ? <Results /> : null}
+        </div>
         <h3>{strMeal}</h3>
         <a href={"https://www.themealdb.com/meal/" + idMeal} target="_blank">
           Ingredients
